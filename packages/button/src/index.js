97f4e578777676be6713078d9db1dad74929d112
Carlo Carpio <carlo@omh.sg>
2019-06-30 14:35:04 +0800

    Initialize project

---
+++ packages/button/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components';
+import Button from '@material-ui/core/Button'
+import { spacing } from 'theme';
+
+const TestButton = () => {
+  return (
+    <Button>12323232323</Button>
+  )
+}
+
+export default TestButton;
